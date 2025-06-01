a9e339ec013959d050d22a21d947f0be29578326
Dimitar Pashev <dim.pashev@gmail.com>
2025-06-01 12:11:06 +0300

    add Dockerfile

--- react_clerk/src/App.jsx
+++ react_clerk/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./Navbar";
-import { useAuth } from "@clerk/clerk-react";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 function App() {
     const [count, setCount] = useState(0);
