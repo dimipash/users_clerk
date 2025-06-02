96654449b22f4b63205e9996618b9e51fbc60d24
Dimitar Pashev <dim.pashev@gmail.com>
2025-06-02 20:35:57 +0300

    Clerk SDK for Python in Django

--- django_clerk/src/pashihome/api.py
+++ django_clerk/src/pashihome/api.py
@@ -1,5 +1,26 @@
+import os
+import httpx
+from clerk_backend_api import Clerk
+from clerk_backend_api.jwks_helpers import (
+    authenticate_request,
+    AuthenticateRequestOptions,
+)
+
+from django.conf import settings
 from django.http import JsonResponse
+
+CLERK_SECRET_KEY = settings.CLERK_SECRET_KEY
+
+
+
 
 
 def hello_world_api_view(request):
+    sdk = Clerk(bearer_auth=CLERK_SECRET_KEY)
+    request_state = sdk.authenticate_request(
+        request, AuthenticateRequestOptions(authorized_parties=["http://localhost:3002"])
+    )
+    payload = request_state.payload
+    clerk_user_id = payload.get("sub")
+    print(clerk_user_id, request_state.is_signed_in)
     return JsonResponse({"message": "Hello, world!"})
