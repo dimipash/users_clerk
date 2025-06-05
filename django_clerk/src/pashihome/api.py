79cff23ba0d78038fb7cb2be3fce6dfba3ebdd08
Dimitar Pashev <dim.pashev@gmail.com>
2025-06-05 17:31:22 +0300

    middleware for request user and Clerk

--- django_clerk/src/pashihome/api.py
+++ django_clerk/src/pashihome/api.py
@@ -6,16 +6,13 @@ CLERK_SECRET_KEY = settings.CLERK_SECRET_KEY
 
 
 def hello_world_api_view(request):
-    clerk_user_id = myclerk.get_clerk_user_id_from_request(request)
-    if not clerk_user_id:
-        return JsonResponse({"message": "Unauthorized"}, status=401)
-    user, created = myclerk.get_clerk_user(clerk_user_id)
-
+    if not request.user.is_authenticated:
+        return JsonResponse({"detail": "Login Required"}, status=400)
+    # print(request.user)
+    user = request.user
     return JsonResponse(
         {
             "first_name": user.first_name,
-            "last_name": user.last_name,
-            "created": created,
-            "message": "Hello World!",
+            "id": user.id,
         }
     )
--- django_clerk/src/pashihome/settings.py
+++ django_clerk/src/pashihome/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "helpers.myclerk.middleware.ClerkAuthMiddleware",
 ]
 
 ROOT_URLCONF = "pashihome.urls"
